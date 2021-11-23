b94515a64a4b86770026de30eb2cf992a1785ae2
cturco44 <cturco@umich.edu>
2021-11-23 00:59:31 -0500

    token working channels

--- api/token_auth.py
+++ api/token_auth.py
@@ -2,7 +2,18 @@
 from channels.auth import AuthMiddlewareStack
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import AnonymousUser
+from channels.db import database_sync_to_async
 
+
+@database_sync_to_async
+def get_token(headers):
+    try:
+        token_name, token_key = headers[b'authorization'].decode().split()
+        if token_name == 'Token':
+            token = Token.objects.get(key=token_key)
+            return token.user
+    except Token.DoesNotExist:
+        return AnonymousUser()
 
 class TokenAuthMiddleware:
     """
@@ -12,16 +23,10 @@ class TokenAuthMiddleware:
     def __init__(self, inner):
         self.inner = inner
 
-    def __call__(self, scope):
+    async def __call__(self, scope, recieve, send):
         headers = dict(scope['headers'])
         if b'authorization' in headers:
-            try:
-                token_name, token_key = headers[b'authorization'].decode().split()
-                if token_name == 'Token':
-                    token = Token.objects.get(key=token_key)
-                    scope['user'] = token.user
-            except Token.DoesNotExist:
-                scope['user'] = AnonymousUser()
-        return self.inner(scope)
+            scope['user'] = await get_token(headers)
+        return await self.inner(scope, recieve, send)
 
 TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(AuthMiddlewareStack(inner))
--- mnky_chat/asgi.py
+++ mnky_chat/asgi.py
@@ -1,7 +1,7 @@
 # mysite/asgi.py
 import os
 
-from channels.auth import AuthMiddlewareStack
+from api.token_auth import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import api.routing
@@ -10,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnky_chat.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
+  "websocket": TokenAuthMiddlewareStack(
         URLRouter(
             api.routing.websocket_urlpatterns
         )
