be165ce50d2874004935e27563d3fa9fd2e05c3d
cturco44 <cturco@umich.edu>
2021-11-23 06:58:43 -0500

    pre testing

--- api/views/groups.py
+++ api/views/groups.py
@@ -5,9 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from api.models import Chat, DirectChat, MemberOf, Message, DirectMessage
+from api.models import Chat, DirectChat, DirectMessageLike, MemberOf, Message, DirectMessage, MessageLike
 from django.contrib.gis.geos import Point
-from itertools import chain
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 
@@ -20,7 +19,11 @@ def active_chats(request):
     
     active_chats = Chat.objects.filter(polygon__covers=current_location)
     if request.user.is_authenticated:
-        active_direct_chats = DirectChat.objects.filter(Q(user1=request.user) | Q(user2=request.user))
+        chat_query = DirectChat.objects.filter(Q(user1=request.user) | Q(user2=request.user))
+        active_direct_chats = []
+        for chat in chat_query:
+            if DirectMessage.objects.filter(chat=chat).count() != 0:
+                active_direct_chats.append(chat)
     else:
         active_direct_chats = []
     
@@ -60,7 +63,7 @@ def active_chats(request):
     
     for chat in active_direct_chats:
         try:
-            recent_message = DirectMessage.objects.latest('timestamp')
+            recent_message = DirectMessage.objects.filter(chat=chat).latest('timestamp')
             recent_message_content = recent_message.content
             recent_message_timestamp = recent_message.timestamp
         except:
@@ -163,3 +166,194 @@ def create_chat(request):
         "image": new_chat.image.url,
     }
     return Response(return_dict, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_messages(request):
+    try:
+        current_location = Point(float(request.data['long']), float(request.data['lat']), srid=4326)
+        chat = Chat.objects.get(chat_id=request.data['chat_id'], polygon__covers=current_location)
+        type = "Chat"
+    except:
+        pass
+    try:
+        chat = DirectChat.objects.get(chat_id=request.data['chat_id'])
+        type = "DirectChat"
+    except:
+        return Response({"error": "invalid chat id"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if type == "Chat":
+        try:
+            MemberOf.objects.get(chat=chat, user=request.user)
+        except:
+            if chat.password:
+                if not request.data['password']:
+                    return Response({"error": "no password"}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    if request.data['password'] != chat.password:
+                        return Response({"Error": "invalid, password"}, status=status.HTTP_403_FORBIDDEN)
+            MemberOf.objects.create(chat=chat, user=request.user)
+        if 'message_id' in request.data:
+            try:
+                cutoff = Message.objects.get(message_id=request.data['message_id'])[:20]
+                query = Message.objects.filter(chat=chat, timestamp__lt=cutoff.timestamp).order_by('-timestamp')
+            except:
+                return Response({"Error": "Invalid message id"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            query = Message.objects.filter(chat=chat).order_by('-timestamp')[:20]
+    
+    else:
+        if chat.user1 != request.user and chat.user2 != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        if 'message_id' in request.data:
+            try:
+                cutoff = DirectMessage.objects.get(message_id=request.data['message_id'])[:20]
+                query = Message.objects.filter(chat=chat, timestamp__lt=cutoff.timestamp).order_by('-timestamp')
+            except:
+                return Response({"Error": "Invalid message id"}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            query = Message.objects.filter(chat=chat).order_by('-timestamp')[:20]
+    
+    message_list = []
+    for message in query:
+        likes = []
+        if type == "Chat":
+            likes = MessageLike.objects.filter(message=message)
+        else:
+            likes = DirectMessageLike.objects.filter(message=message)
+        for like in likes:
+            likes.append({
+                "first_name": like.user.first_name,
+                "last_name": like.user.last_name,
+                "username": like.user.username,
+                "profile_pic": like.user.profile_pic.url
+            })
+        if message.file:
+            message_type = "file"
+            content = message.file.url
+        else:
+            message_type = "message"
+            content = message.content
+        x = {
+            "type": message_type,
+            "first_name": message.sender.first_name,
+            "last_name": message.sender.last_name,
+            "username": message.sender.username,
+            "profile_pic": message.sender.profile_pic,
+            "content": content,
+            "likes": likes
+        }
+        message_list.append(x)
+    return Response({"messages": message_list}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def get_files(request):
+    try:
+        current_location = Point(float(request.data['long']), float(request.data['lat']), srid=4326)
+        chat = Chat.objects.get(chat_id=request.data['chat_id'], polygon__covers=current_location)
+        type = "Chat"
+    except:
+        pass
+    try:
+        chat = DirectChat.objects.get(chat_id=request.data['chat_id'])
+        type = "DirectChat"
+    except:
+        return Response({"error": "invalid chat id"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if type == "Chat":
+        try:
+            MemberOf.objects.get(chat=chat, user=request.user)
+        except:
+             return Response(status=status.HTTP_403_FORBIDDEN)
+        query = Message.objects.filter(chat=chat, file__isnull=False)
+    else:
+        if chat.user1 != request.user and chat.user2 != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        query = DirectMessage.objects.filter(chat=chat, file__isnull=False)
+    
+    file_list = []
+    for message in query:
+        file_list.append({
+            "file": message.file.url
+        })
+    
+    return Response({"files": file_list}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def chat_log(request):
+    try:
+        current_location = Point(float(request.data['long']), float(request.data['lat']), srid=4326)
+    except:
+        return Response({"error": "invalid location"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    query = MemberOf.objects.filter(user=request.user).filter(~Q(chat__polygon__covers=current_location))
+
+    chat_log = []
+    for item in query:
+        most_recent_message = Message.objects.filter(chat=item.chat).latest('timestamp')
+        chat_log.append({
+            "name": item.chat.name,
+            "image": item.chat.image.url,
+            "timestamp": most_recent_message.timestamp
+        })
+    
+    chat_log = sorted(chat_log, key=lambda x: (x['timestamp'] is None, x['timestamp']), reverse=True)
+
+    for item in chat_log:
+        item["timestamp"] = str(item["timestamp"])
+    
+    return Response({"chat_log": chat_log}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def direct_message(request):
+    try:
+        user = User.objects.filter(username=request.data["username"])
+    except:
+        return Response({"error": "invalid username"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    direct_chat = DirectChat.objects.create(user1=request.user, user2=user)
+
+    return Response({"chat_id": direct_chat.chat_id}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def leave_chat(request):
+    try:
+        chat = Chat.objects.get(chat_id=request.data['chat_id'])
+    except:
+        return Response({"error": "could not find chat"}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        x = MemberOf.objects.get(user=request.user, chat=chat)
+        x.delete()
+    except:
+        return Response({"error": "user is not a member of this chat"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if chat.owner == request.user:
+        other_users = MemberOf.objects.filter(chat=chat).filter(~Q(user=request.user))
+        if not other_users:
+            chat.delete()
+        else:
+            chat.owner = other_users[0]
+            chat.save()
+    return Response({}, status.HTTP_200_OK)
+            
+
+
+
+
+
+
+
+
+    
+
