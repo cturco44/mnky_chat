42399278d33b932dae377ec2423c8ce2ff63f396
cturco44 <cturco@umich.edu>
2021-11-29 20:29:50 -0500

    try redeploy again

--- mnky_chat/settings.py
+++ mnky_chat/settings.py
@@ -18,8 +18,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
