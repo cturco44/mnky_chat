f829c0dd82e5c20cfab0ecef2ea42ce8eb0814ff
cturco44 <cturco@umich.edu>
2021-12-02 04:18:45 -0500

    temp change agsi.py

--- mnky_chat/asgi.py
+++ mnky_chat/asgi.py
@@ -4,6 +4,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mnky_chat.settings")
 agsi_routes = get_asgi_application()
+
 from api.token_auth import TokenAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
@@ -12,7 +13,7 @@ import api.routing
 
 
 application = ProtocolTypeRouter({
-  "http": agsi_routes,
+#   "http": agsi_routes,
   "websocket": TokenAuthMiddlewareStack(
         URLRouter(
             api.routing.websocket_urlpatterns
