266e1b99daf20f807cdb173c985df7f8d95d9314
cturco44 <cturco@umich.edu>
2021-12-04 16:08:18 -0500

    add to allowed hosts

--- mnky_chat/settings.py
+++ mnky_chat/settings.py
@@ -39,17 +39,6 @@ else:
     STATIC_URL = '/static/'
     MEDIA_URL = '/media/'
 
-    ### Django storages - use in production
-    
-    
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-        "hosts": [('django-channels-redis.j4caak.ng.0001.use1.cache.amazonaws.com', 6379)]
-        },
-    }
-}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -60,7 +49,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-n%w0y3@j5bi!)lg6)6mw5l@p@euj2iw1y&+7yebad&dkhf8xb_'
 
-ALLOWED_HOSTS = ['52.3.3.112','172.31.33.156','172.31.33.156','mnky-chat-script2.us-east-1.elasticbeanstalk.com','mkny-chat-new-settings.us-east-1.elasticbeanstalk.com','35.2.13.232', '127.0.0.1', '172.31.37.2', '172.31.12.212', 'mnky-chat-fix-script.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['mnky-chat.com','52.3.3.112','172.31.33.156','172.31.33.156','mnky-chat-script2.us-east-1.elasticbeanstalk.com','mkny-chat-new-settings.us-east-1.elasticbeanstalk.com','35.2.13.232', '127.0.0.1', '172.31.37.2', '172.31.12.212', 'mnky-chat-fix-script.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
